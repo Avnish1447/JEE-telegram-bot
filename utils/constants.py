f58e0a679d735f035017243caf03f82204e8a51e
Avnish <avnishagrawal1447+github@gmail.com>
2025-11-29 18:59:33 +0530

    Add coaching configurations

---
+++ utils/constants.py
@@ -0,0 +1,44 @@
+COACHINGS = {
+    "pw": {
+        "name": "PW", 
+        "batches": ["batch_1", "batch_2"],
+        "group_id": -1001234567890  # Replace with actual Telegram group ID
+    },
+    "allen": {
+        "name": "Allen", 
+        "batches": ["batch_1", "batch_2"],
+        "group_id": -1001234567891  # Replace with actual Telegram group ID
+    },
+    "aakash": {
+        "name": "Aakash", 
+        "batches": ["batch_1", "batch_2"],
+        "group_id": -1001234567892  # Replace with actual Telegram group ID
+    },
+    "resonance": {
+        "name": "Resonance", 
+        "batches": ["batch_1", "batch_2"],
+        "group_id": -1001234567893  # Replace with actual Telegram group ID
+    },
+    "fiitjee": {
+        "name": "FIITJEE", 
+        "batches": ["batch_1", "batch_2"],
+        "group_id": -1001234567894  # Replace with actual Telegram group ID
+    },
+}
+
+INVITE_MAP = {
+    "https://t.me/+PLACEHOLDER_PW_BATCH1": ("pw", "batch_1"),
+    "https://t.me/+PLACEHOLDER_PW_BATCH2": ("pw", "batch_2"),
+    
+    "https://t.me/+PLACEHOLDER_ALLEN_BATCH1": ("allen", "batch_1"),
+    "https://t.me/+PLACEHOLDER_ALLEN_BATCH2": ("allen", "batch_2"),
+
+    "https://t.me/+PLACEHOLDER_AAKASH_BATCH1": ("aakash", "batch_1"),
+    "https://t.me/+PLACEHOLDER_AAKASH_BATCH2": ("aakash", "batch_2"),
+
+    "https://t.me/+PLACEHOLDER_RESO_BATCH1": ("resonance", "batch_1"),
+    "https://t.me/+PLACEHOLDER_RESO_BATCH2": ("resonance", "batch_2"),
+
+    "https://t.me/+PLACEHOLDER_FIITJEE_BATCH1": ("fiitjee", "batch_1"),
+    "https://t.me/+PLACEHOLDER_FIITJEE_BATCH2": ("fiitjee", "batch_2"),
+}
